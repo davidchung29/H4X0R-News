ed3bb15612c48f2110be9d8da9d3ea1757a0fe9e
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2021-01-22 14:33:44 -0800

    created basic List and Navigation View

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(posts){ post in
+                Text(post.title)
+                
+            }
+            .navigationTitle("H4X0R News")
+        }
+        
     }
 }
 
@@ -19,3 +25,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable{
+    let id: String
+    let title: String
+    
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hola"),
+    Post(id: "3", title: "Nice")
+]
